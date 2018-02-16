d8a2fd5890d713e7e3d0474a4af44b91a2a0b10a
Vimal <288440vimal@gmail.com>
2018-02-16 07:55:47 -0500

    Correct forEach quiz

--- Javascript files/arrays-foreach-quiz.js
+++ Javascript files/arrays-foreach-quiz.js
@@ -9,16 +9,16 @@
  *  - you can use `console.log` to verify the `test` variable when you're finished looping
  */
 
-var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
-    19, 300, 3775, 299, 36, 209, 148, 169, 299,
-    6, 109, 20, 58, 139, 59, 3, 1, 139
-];
+ var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+     19, 300, 3775, 299, 36, 209, 148, 169, 299,
+     6, 109, 20, 58, 139, 59, 3, 1, 139
+ ];
 
-// Write your code here
-test.foreach(function(element,index,array){
-    if(element%3===0){
-        array[index] += 100;
-    }
-});
+ // Write your code here
+ test.forEach(function(element,index,array){
+     if(element%3===0){
+         array[index] += 100;
+     }
+ });
 
-console.log(test);
+ console.log(test);
