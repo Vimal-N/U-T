76eeb897d5625383ddc82e30a6cc26a3a8f83d55
Vimal <288440vimal@gmail.com>
2018-02-16 19:19:17 -0500

    Objects-bank quiz

---
+++ Javascript files/Objects-Bank Accounts.js
@@ -0,0 +1,33 @@
+/*
+ * Programming Quiz: Bank Accounts 1 (7-3)
+ */
+
+var savingsAccount = {
+    balance: 1000,
+    interestRatePercent: 1,
+    deposit: function addMoney(amount) {
+        if (amount > 0) {
+            savingsAccount.balance += amount;
+        }
+    },
+    withdraw: function removeMoney(amount) {
+        var verifyBalance = savingsAccount.balance - amount;
+        if (amount > 0 && verifyBalance >= 0) {
+            savingsAccount.balance -= amount;
+        }
+    },
+    printAccountSummary: function (){
+      return("Welcome!\nYour balance is currently $"+this.balance+" and your interest rate is "+this.interestRatePercent+"%.")
+    }
+};
+
+console.log(savingsAccount.printAccountSummary());
+
+
+/*
+THINGS LEARNED HERE:
+TO REFER TO VARIABLE IN THE SAME OBJECT, WE CAN USE this.
+
+
+
+*/
